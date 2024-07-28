b4c61990e11622de0e2c954c2b4bf0b5119e9ba7
Nils Robinet <nils.robinet@gmail.com>
2024-07-28 19:52:31 +0200

    Function for subtraction the average face from a bathc of Eigenfaces

--- Eigenface/cython/Eigenface.pxd
+++ Eigenface/cython/Eigenface.pxd
@@ -3,3 +3,8 @@ cdef extern from "EigenfaceMain.h" namespace "Eigenface":
     cdef cppclass Eigenface:
         Eigenface()
         void getAverageFace(float[256*256*8], float[256*256])
+        void getDeviationFromAverageFace(float[dimX*dimY*batch])
+
+    C dimX "Eigenface::Eigenface::dimX"
+    C dimY "Eigenface::Eigenface::dimY"
+    C batch "Eigenface::Eigenface::batch"
--- Eigenface/src/EigenfaceMain.cpp
+++ Eigenface/src/EigenfaceMain.cpp
@@ -20,4 +20,19 @@ namespace Eigenface {
         auto tmp = Math::averageVector(facesFlat);
         std::memcpy(avgFace, tmp.data(), dimX * dimY * sizeof(float));
     }
+    
+    void Eigenface::getDeviationFromAverageFace(float faces[dimX*dimY*batchSize]){
+        float avgFace[dimX*dimY];
+        this->getAverageFace(faces,avgFace);
+        //float facesIterable[dimX][dimY][batchSize] = faces;
+        for (auto i = 0U; i < batchSize; i++) {
+            for (auto x = 0U; x < dimX; x++){
+                for (auto y = 0U; y < dimY; y++){
+                    // Dangerous pointer arithmetic
+                    faces[x*dimX+y+i*batchSize] = faces[x*dimX+y+i*batchSize] - avgFace[x*dimX+y];
+                    //facesIterable[x][y][i] = facesIterable[x][y][i] - avgFace[x][y];
+                }
+            }
+        }
+    }
 }
