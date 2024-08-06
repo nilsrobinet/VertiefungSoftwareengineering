fe5e3b5abc6faf2b0a7e2b3e95caf55eebb5b47e
Nils Robinet <nils.robinet@gmail.com>
2024-08-06 18:14:46 +0200

    Add references to tools chapter

--- build_scripts/makedoc.sh
+++ build_scripts/makedoc.sh
@@ -16,7 +16,13 @@ cd $ROOT_DIR
 
 cd docu/ausarbeitung/
 pdflatex --output-directory=$ROOT_DIR/build/docu main.tex
-biber --output-directory=$ROOT_DIR/build/docu main.tex
+cp main.bib $ROOT_DIR/build/docu
+cd $ROOT_DIR/build/docu
+bibtex main
+cd $ROOT_DIR/docu/ausarbeitung
+echo "###################2###########################"
+echo `pwd`
+echo `ls`
 pdflatex --output-directory=$ROOT_DIR/build/docu main.tex
 pdflatex --output-directory=$ROOT_DIR/build/docu main.tex
 cd $ROOT_DIR
