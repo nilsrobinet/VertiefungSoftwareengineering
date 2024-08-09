f6ca9b2c4a41d0d2c13c74c2891c5840bfebe6c3
Nils Robinet <nils.robinet@gmail.com>
2024-08-09 14:50:03 +0200

    add deployment section to docu

--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -20,6 +20,6 @@ rsync -e "ssh -o StrictHostKeyChecking=no" build/demo_docker.tar root@$DEPLOY_HO
 ssh -o StrictHostKeyChecking=no root@$DEPLOY_HOST -i /root/.ssh/id_ed25519 \
 'echo "successfully connected" &&\
 docker load -i "/tmp/demo_docker.tar" &&\
-docker run -d --name --publish 8080:8080 webapp_demo_container webapp_demo_docker'
+docker run -d -p 8080:8080 --name webapp_demo_container webapp_demo_docker'
 
 #$(docker images --format="{{.Repository}}:{{.ID}}" | grep "webapp_demo_docker" | awk "{print $2}")'
